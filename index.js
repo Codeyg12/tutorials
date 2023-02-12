c2b46bc798f256a2d4a1068914e28579c73aa487
Codeyg12 <codey.gallup@gmail.com>
2023-02-11 18:49:38 -0700

    debugging mode to take away quad section

--- index.js
+++ index.js
@@ -2,21 +2,58 @@
 // first make to follow mouse
 // make follow camera
 
-const ctx = canvas.getContext('2d')
-canvas.width = 1000;
-canvas.height = 1000;
+const ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 500;
+let clearing = false;
 
-ctx.beginPath()
-ctx.fillStyle = 'black'
-ctx.moveTo(0, 500)
-ctx.lineTo(1000, 500)
-ctx.stroke()
-ctx.beginPath()
-ctx.fillStyle = 'black'
-ctx.moveTo(500, 0)
-ctx.lineTo(500, 1000)
-ctx.stroke()
+// Quads
+function debug() {
+  if (clearing) {
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.moveTo(0, 250);
+    ctx.lineTo(500, 250);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.moveTo(250, 0);
+    ctx.lineTo(250, 500);
+    ctx.stroke();
+  } else {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  }
+}
 
-canvas.addEventListener('click', (e) => {
-    console.log(`${e.offsetX} ${e.offsetY}`)
-})
+// Top head
+function drawHead() {
+    ctx.beginPath();
+ctx.moveTo(85, 250);
+ctx.quadraticCurveTo(75, 75, 250, 60);
+ctx.moveTo(415, 250);
+ctx.quadraticCurveTo(425, 75, 250, 60);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(85, 250);
+ctx.quadraticCurveTo(75, 425, 250, 440);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(415, 250);
+ctx.quadraticCurveTo(425, 415, 250, 440);
+ctx.stroke();
+}
+
+canvas.addEventListener("click", (e) => {
+  console.log(`${e.offsetX} ${e.offsetY}`);
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "d") {
+    clearing = !clearing;
+    debug()
+    drawHead()
+  }
+});
+
+drawHead()

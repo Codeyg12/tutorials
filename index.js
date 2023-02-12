aa627045de33f8a7ecd89475d6fce6eb18729160
Codeyg12 <codey.gallup@gmail.com>
2023-02-11 18:12:56 -0700

    all files added and started

---
+++ index.js
@@ -0,0 +1,8 @@
+// canvas draw eyes with beginPath, moveTo, quadraticCurveTo,stroke
+// first make to follow mouse
+// make follow camera
+
+const ctx = canvas.getContext('2d')
+canvas.width = 1000;
+canvas.height = 1000;
+console.log(canvas)

aa5dedcaa4929c51594526842021e2b2d02fa150
Codeyg12 <codey.gallup@gmail.com>
2023-02-11 18:24:04 -0700

    function to find coords and seperate into quads

--- index.js
+++ index.js
@@ -5,4 +5,18 @@
 const ctx = canvas.getContext('2d')
 canvas.width = 1000;
 canvas.height = 1000;
-console.log(canvas)
+
+ctx.beginPath()
+ctx.fillStyle = 'black'
+ctx.moveTo(0, 500)
+ctx.lineTo(1000, 500)
+ctx.stroke()
+ctx.beginPath()
+ctx.fillStyle = 'black'
+ctx.moveTo(500, 0)
+ctx.lineTo(500, 1000)
+ctx.stroke()
+
+canvas.addEventListener('click', (e) => {
+    console.log(`${e.offsetX} ${e.offsetY}`)
+})

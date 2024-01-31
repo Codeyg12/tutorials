1630b6e82fb543c89ed56ef1ec53c92e85388101
Codeyg12 <codey.gallup@gmail.com>
2024-01-30 18:31:49 -0600

    starting to grab ids and a switch

---
+++ two_sum_visual/index.js
@@ -0,0 +1,36 @@
+const bruteForceBtn = document.getElementById('brute-force-btn')
+const bruteForceNumber = document.querySelector('#brute-force-output > .numbers-array')
+const bruteForceText = document.querySelector('#brute-force-output > .results-text')
+let currentNum
+let currentComplement
+
+let twoSum = function(nums, target) {
+    if (nums.length === 2) return [0, 1]
+
+    for (let i = 0; i < nums.length; i++) {
+        const currentEl = nums[i]
+        for (let j = i + 1; j < nums.length; j ++) {
+            if (currentEl + j === target) return [i, j]     
+        }
+    }
+};
+
+const getClassName = num => {
+    switch (num) {
+        case currentNum:
+            return "class='current-num"
+            
+            break;
+        case currentComplement:
+            return "class='complement-num'"
+            break;
+        default:
+            return ""
+            break;
+    }
+}
+
+twoSum([2,7,11,15], 9)
+twoSum([3, 2, 4], 6)
+twoSum([3, 3], 6)
+twoSum([3, 5, 6, 5, 3], 10)

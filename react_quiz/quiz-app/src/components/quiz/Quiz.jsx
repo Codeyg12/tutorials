12ec3037895edebc0654a536312a3966e8f2628b
Codeyg12 <codey.gallup@gmail.com>
2024-02-06 17:26:40 -0600

    styling container

--- react_quiz/quiz-app/src/components/quiz/Quiz.jsx
+++ react_quiz/quiz-app/src/components/quiz/Quiz.jsx
@@ -3,7 +3,19 @@ import './Quiz.css'
 const Quiz = () => {
     return (
         <>
-        <h1>HELLO GUV</h1>
+        <div className="container">
+            <h1>Quiz App</h1>
+            <hr />
+            <h2>What device is needed to connect to the internet?</h2>
+            <ul>
+                <li>Modem</li>
+                <li>Router</li>
+                <li>LAN Cable</li>
+                <li>Pen Drive</li>
+            </ul>
+            <button>Next</button>
+            <div className="index">1 of 5 questions</div>
+        </div>
         </>
     )
 }

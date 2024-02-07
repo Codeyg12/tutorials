20a40dc98bbc4199e934d00891a3106e85da8438
Codeyg12 <codey.gallup@gmail.com>
2024-02-06 21:30:53 -0600

    classes for wrong and correct answers

--- react_quiz/quiz-app/src/components/quiz/Quiz.jsx
+++ react_quiz/quiz-app/src/components/quiz/Quiz.jsx
@@ -1,17 +1,31 @@
+import { useState } from 'react'
 import './Quiz.css'
+import { data } from '../../assets/data'
 
 const Quiz = () => {
+
+    let [index, setIndex] = useState(2)
+    let [question, setQuestion] = useState(data[index])
+
+    const checkAnswer = (e, answer) => {
+        if (question.ans==answer) {
+            e.target.classList.add('correct')
+        } else {
+            e.target.classList.add('wrong')
+        }
+    }
+
     return (
         <>
         <div className="container">
             <h1>Quiz App</h1>
             <hr />
-            <h2>What device is needed to connect to the internet?</h2>
+            <h2>{index+1}. {question.question}</h2>
             <ul>
-                <li>Modem</li>
-                <li>Router</li>
-                <li>LAN Cable</li>
-                <li>Pen Drive</li>
+                <li onClick={(e)=>checkAnswer(e,1)}>{question.option1}</li>
+                <li onClick={(e)=>checkAnswer(e,2)}>{question.option2}</li>
+                <li onClick={(e)=>checkAnswer(e,3)}>{question.option3}</li>
+                <li onClick={(e)=>checkAnswer(e,4)}>{question.option4}</li>
             </ul>
             <button>Next</button>
             <div className="index">1 of 5 questions</div>

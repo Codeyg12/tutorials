0113431ec9947896856adf6eb6d9abf4d17607fb
Codeyg12 <codey.gallup@gmail.com>
2024-02-11 10:28:39 -0600

    setting up buttons to change color and clear the canvas

--- python_whiteboard/whiteboard.py
+++ python_whiteboard/whiteboard.py
@@ -12,7 +12,7 @@ def draw(e):
     global is_drawing, prev_x, prev_y
     if is_drawing:
         current_x, current_y = e.x, e.y
-        canvas.create_line(prev_x, prev_y, current_x, current_y, fill=drawing_color, width=line_width, capstyle=tl.ROUND, smooth=True)
+        canvas.create_line(prev_x, prev_y, current_x, current_y, fill=drawing_color, width=line_width, capstyle=tk.ROUND, smooth=True)
         prev_x, prev_y = current_x, current_y
 
 def stop_draw(e):
@@ -40,3 +40,12 @@ drawing_color = 'black'
 line_width = 2
 
 root.geometry('800x600')
+
+controls_frame = tk.Frame(root)
+controls_frame.pack(side='top', fill='x')
+
+color_button = tk.Button(controls_frame, text='Change Color', command=change_pen_color)
+clear_button = tk.Button(controls_frame, text='Clear Canvas', command=lambda: canvas.delete('all'))
+
+color_button.pack(side='left', padx=5, pady=5)
+clear_button.pack(side='left', padx=5, pady=5)

f361ff9fc373379ebea75fdc2434696d1128bb18
Codeyg12 <codey.gallup@gmail.com>
2024-02-10 19:42:17 -0600

    start draw function to to record location and line style

--- python_whiteboard/whiteboard.py
+++ python_whiteboard/whiteboard.py
@@ -1,2 +1,9 @@
 import tkinter as tk
 from tkinter.colorchooser import askcolor
+
+def start_drawing(e):
+    global is_drawing, prev_x, prev_y
+    if is_drawing:
+        current_x, current_y = e.x, e.y
+        canvas.create_line(prev_x, prev_y, current_x, current_y, fill=drawing_color, width=line_width, capstyle=tk.ROUND, smooth=True)
+        prev_x, prev_y = current_x, current_y

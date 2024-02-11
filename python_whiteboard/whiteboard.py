604d7748af7a9aed77023e24ed5a23ffb8cd0152
Codeyg12 <codey.gallup@gmail.com>
2024-02-10 19:46:59 -0600

    function to stop drawing

--- python_whiteboard/whiteboard.py
+++ python_whiteboard/whiteboard.py
@@ -7,3 +7,14 @@ def start_drawing(e):
         current_x, current_y = e.x, e.y
         canvas.create_line(prev_x, prev_y, current_x, current_y, fill=drawing_color, width=line_width, capstyle=tk.ROUND, smooth=True)
         prev_x, prev_y = current_x, current_y
+
+def draw(e):
+    global is_drawing, prev_x, prev_y
+    if is_drawing:
+        current_x, current_y = e.x, e.y
+        canvas.create_line(prev_x, prev_y, current_x, current_y, fill=drawing_color, width=line_width, capstyle=tl.ROUND, smooth=True)
+        prev_x, prev_y = current_x, current_y
+
+def stop_draw(e):
+    global is_drawing
+    is_drawing = False

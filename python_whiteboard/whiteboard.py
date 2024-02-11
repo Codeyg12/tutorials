0c2b9a2984345351e5fc137e7fb28af66068f940
Codeyg12 <codey.gallup@gmail.com>
2024-02-11 10:33:51 -0600

    setting up adjusted for line width

--- python_whiteboard/whiteboard.py
+++ python_whiteboard/whiteboard.py
@@ -49,3 +49,10 @@ clear_button = tk.Button(controls_frame, text='Clear Canvas', command=lambda: ca
 
 color_button.pack(side='left', padx=5, pady=5)
 clear_button.pack(side='left', padx=5, pady=5)
+
+line_width_label = tk.Label(controls_frame, text='Line Width:')
+line_width_label.pack(side='left', padx=5, pady=5)
+
+line_width_slider = tk.Scale(controls_frame, from_=1, to=10, orient='horizontal', command=lambda val: change_line_width(val))
+line_width_slider.set(line_width)
+line_width_slider.pack

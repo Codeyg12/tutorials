d57ee7645f37e1f31e359f99ad1b5c48ecbf64ff
Codeyg12 <codey.gallup@gmail.com>
2024-02-10 23:12:19 -0600

    creating a window

--- python_whiteboard/whiteboard.py
+++ python_whiteboard/whiteboard.py
@@ -18,3 +18,25 @@ def draw(e):
 def stop_draw(e):
     global is_drawing
     is_drawing = False
+
+def change_pen_color():
+    global drawing_color
+    color = askcolor()[1]
+    if color:
+        drawing_color = color
+
+def change_line_width(value):
+    global line_width
+    line_width = int(value)
+
+root = tk.Tk()
+root.title('Whiteboard App')
+
+canvas = tk.Canvas(root, bg='white')
+canvas.pack(fill='both', expand=True)
+
+is_drawing = False
+drawing_color = 'black'
+line_width = 2
+
+root.geometry('800x600')

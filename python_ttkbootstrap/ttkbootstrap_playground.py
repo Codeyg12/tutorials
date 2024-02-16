47209c553c66abd19f4c10772a34592e455b09ac
Codeyg12 <codey.gallup@gmail.com>
2024-02-16 15:31:40 -0700

    giving the button a function

--- python_ttkbootstrap/ttkbootstrap_playground.py
+++ python_ttkbootstrap/ttkbootstrap_playground.py
@@ -7,12 +7,21 @@ root = tb.Window(themename='solar')
 root.title('TTK Bootstrap')
 root.geometry('500x360')
 
+counter = 0
+def changer():
+    global counter
+    counter += 1
+    if counter % 2 == 0:
+        label.config(text='Hello World')
+    else:
+        label.config(text='Goodbye World')
+
 # Label
 label = tb.Label(text='Hello World', font=('Helvetica', 28), bootstyle='danger')
 label.pack(pady=50)
 
 # Button
-button = tb.Button(text='Click here', bootstyle='primary')
+button = tb.Button(text='Click here', bootstyle='primary, outline', command=changer)
 button.pack(pady=75)
 
 

3979ec0a5c9a311742b98fa1477ada6252f32040
Codeyg12 <codey.gallup@gmail.com>
2024-02-16 15:41:45 -0700

    adding checkbutton with function

--- python_ttkbootstrap/ttkbootstrap_playground.py
+++ python_ttkbootstrap/ttkbootstrap_playground.py
@@ -7,22 +7,38 @@ root = tb.Window(themename='solar')
 root.title('TTK Bootstrap')
 root.geometry('500x360')
 
-counter = 0
-def changer():
-    global counter
-    counter += 1
-    if counter % 2 == 0:
-        label.config(text='Hello World')
-    else:
-        label.config(text='Goodbye World')
+# # Button Function
+# counter = 0
+# def changer():
+#     global counter
+#     counter += 1
+#     if counter % 2 == 0:
+#         label.config(text='Hello World')
+#     else:
+#         label.config(text='Goodbye World')
 
-# Label
-label = tb.Label(text='Hello World', font=('Helvetica', 28), bootstyle='danger')
-label.pack(pady=50)
+# # Label
+# label = tb.Label(text='Hello World', font=('Helvetica', 28), bootstyle='danger')
+# label.pack(pady=50)
 
-# Button
-button = tb.Button(text='Click here', bootstyle='primary, outline', command=changer)
-button.pack(pady=75)
+# # Button
+# button = tb.Button(text='Click here', bootstyle='primary, outline', command=changer)
+# button.pack(pady=75)
 
+# # Checkbox Function
+# def checker():
+#     if var1.get() == 1: 
+#         checkbox_label.config(text='Checked')
+#     else:
+#         checkbox_label.config(text='Unchecked')
+
+# # Checkbox Label
+# checkbox_label = Label(text='Click the checkbutton below', font=('Helvetica', 18))
+# checkbox_label.pack(pady=(40, 10))
+
+# # Checkbox
+# var1 = IntVar()
+# checkbox = tb.Checkbutton(bootstyle='info', text='Check Me Out', variable=var1, onvalue=1, offvalue=0, command=checker)
+# checkbox.pack(pady=10)
 
 root.mainloop()

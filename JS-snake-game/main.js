3bdb43edd7c4e36f4d4cde02ee589d982b41ec9a
Codeyg12 <codey.gallup@gmail.com>
2024-02-18 15:49:40 -0700

    func to move and start game

--- JS-snake-game/main.js
+++ JS-snake-game/main.js
@@ -1,10 +1,17 @@
 const board = document.getElementById('game-board')
 
+let gridSize = 20
 let snake = [{x: 10, y:10}]
+let food = generateFood() 
+let direction = 'right'
+let gameInterval
+let gameSpeedDelay = 200
+let gameStarted = false
 
 function draw() {
     board.innerHTML = ''
     drawSnake()
+    drawFood()
 }
 
 // draw the snake
@@ -20,7 +27,6 @@ function drawSnake() {
 function createGameElement(tag, className) {
     const element = document.createElement(tag)
     element.className = className
-
     return element
 }
 
@@ -28,6 +34,53 @@ function createGameElement(tag, className) {
 function setPosition(element, position) {
     element.style.gridColumn = position.x
     element.style.gridRow = position.y
+}
+
+function drawFood() {
+    const foodEl = createGameElement('div', 'food')
+    setPosition(foodEl, food)
+    board.appendChild(foodEl)
+}
+
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1
+    const y = Math.floor(Math.random() * gridSize) + 1
+    return {x, y}
+}
+
+function move() {
+    const head = { ...snake[0] }
+    switch (direction) {
+        case 'right':
+            head.x++
+            break;
+        case 'left':
+            head.x--
+            break;
+        case 'down':
+            head.y++
+            break;
+        case 'up':
+            head.y--
+            break;
+    }
+
+    snake.unshift(head)
+    
+    if (head.x === food.x && head.y === food.y) {
+        food = generateFood()
+        clearInterval()
+        gameInterval = setInterval(() => {
+            move()
+            draw()
+        }, gameSpeedDelay)
+    } else {
+        snake.pop()
+    }
+}
+
+function startGame() {
+    gameStarted = true
 }
 
 draw()

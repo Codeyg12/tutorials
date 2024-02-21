b208825541fc1be26acbd1001fb846fd21f508c0
Codeyg12 <codey.gallup@gmail.com>
2024-02-20 18:36:23 -0700

    creating temporary func to test segment and point functionality

--- JS-virtual-car/js/primitives/point.js
+++ JS-virtual-car/js/primitives/point.js
@@ -4,6 +4,10 @@ class Point {
     this.y = y;
   }
 
+  equals(point) {
+    return this.x == point.x && this.y == point.y;
+  }
+
   draw(ctx, size = 18, color = "black") {
     const rad = size / 2;
     ctx.beginPath();
@@ -12,4 +16,3 @@ class Point {
     ctx.fill();
   }
 }
-

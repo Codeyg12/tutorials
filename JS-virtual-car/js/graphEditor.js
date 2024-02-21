64280594780f0dfa9c41b7e4ed7c6df6217535d1
Codeyg12 <codey.gallup@gmail.com>
2024-02-20 19:28:44 -0700

    add hovering effect if mouse is near the point

--- JS-virtual-car/js/graphEditor.js
+++ JS-virtual-car/js/graphEditor.js
@@ -14,7 +14,7 @@ class GraphEditor {
     #addEventListeners() {
         this.canvas.addEventListener('mousedown', (e) => {
             const mouse = new Point(e.offsetX, e.offsetY)
-            this.hovered = getNearestPoint(mouse, this.graph.points)
+            this.hovered = getNearestPoint(mouse, this.graph.points, 10)
             if (this.hovered) {
                 this.selected = this.hovered
                 return
@@ -22,10 +22,17 @@ class GraphEditor {
             this.graph.addPoint(mouse)
             this.selected = mouse
         })
+        this.canvas.addEventListener('mousemove', (e) => {
+            const mouse = new Point(e.offsetX, e.offsetY)
+            this.hovered = getNearestPoint(mouse, this.graph.points, 10)
+        })
     }
 
     display() {
         this.graph.draw(this.ctx)
+        if (this.hovered) {
+            this.hovered.draw(this.ctx, { fill: true })
+        }
         if (this.selected) {
             this.selected.draw(this.ctx, { outline: true })
         }

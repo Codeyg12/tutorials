29eb5674caefb6b439e4432fedc9f27617b0d5e7
Codeyg12 <codey.gallup@gmail.com>
2024-02-20 17:41:34 -0700

    creating point and segement classes, testing points

--- JS-virtual-car/js/math/graph.js
+++ JS-virtual-car/js/math/graph.js
@@ -1,1 +1,16 @@
-console.log('hello');
+class Graph {
+  constructor(points = [], segments = []) {
+    this.points = points;
+    this.segments = segments;
+  }
+
+  draw(ctx) {
+    for (const seg of this.segments) {
+      seg.draw(ctx);
+    }
+
+    for (const point of this.points) {
+      point.draw(ctx);
+    }
+  }
+}

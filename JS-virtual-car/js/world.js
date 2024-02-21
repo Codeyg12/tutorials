4930e8cb1b3640ebaa6a477fbcabcc0bd8580ed7
Codeyg12 <codey.gallup@gmail.com>
2024-02-21 13:29:05 -0700

    bases of houses are appearing where space is provided

--- JS-virtual-car/js/world.js
+++ JS-virtual-car/js/world.js
@@ -65,8 +65,22 @@ class World {
           supports.push(new Segment(q1, q2))
         }
       }
+
+      const bases = []
+      for (const seg of supports) {
+        bases.push(new Envelope(seg, this.buildingWidth).poly)
+      }
+
+      for (let i = 0; i < bases.length; i++) {
+        for (let j = i + 1; j < bases.length; j++) {
+          if (bases[i].intersectsPoly(bases[j])) {
+            bases.splice(j, 1)
+            j--
+          }
+        }
+      }
     
-    return supports
+    return bases
   }
 
   draw(ctx) {

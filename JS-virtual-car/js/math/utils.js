40e77e8f5e604dd81caa13c2b9d3867ab4517ed9
Codeyg12 <codey.gallup@gmail.com>
2024-02-21 15:55:17 -0700

    trees added, randomly generated but wont move upon rerender, has a 3d effect

--- JS-virtual-car/js/math/utils.js
+++ JS-virtual-car/js/math/utils.js
@@ -32,11 +32,11 @@ function scale(p, scaler) {
 }
 
 function normalize(p) {
-  return scale(p, 1 / magnitude(p))
+  return scale(p, 1 / magnitude(p));
 }
 
 function magnitude(p) {
-  return Math.hypot(p.x, p.y)
+  return Math.hypot(p.x, p.y);
 }
 
 function translate(loc, angle, offset) {
@@ -55,17 +55,17 @@ function getIntersection(A, B, C, D) {
   const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
   const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
 
-  const eps = 0.001
+  const eps = 0.001;
   if (Math.abs(bottom) != eps) {
-     const t = tTop / bottom;
-     const u = uTop / bottom;
-     if (t >= 0 && t <= 1 && u >= 0 && u <= 1){
-        return {
-           x: lerp(A.x, B.x, t),
-           y: lerp(A.y, B.y, t),
-           offset: t,
-        };
-     }
+    const t = tTop / bottom;
+    const u = uTop / bottom;
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      return {
+        x: lerp(A.x, B.x, t),
+        y: lerp(A.y, B.y, t),
+        offset: t,
+      };
+    }
   }
 
   return null;
@@ -75,6 +75,10 @@ function lerp(a, b, t) {
   return a + (b - a) * t;
 }
 
- function dot(p1, p2) {
+function lerp2D(A, B, t) {
+  return new Point(lerp(A.x, B.x, t), lerp(A.y, B.y, t));
+}
+
+function dot(p1, p2) {
   return p1.x * p2.x + p1.y * p2.y;
 }

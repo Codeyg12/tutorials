913100b41049c3a2f76348cc6cac1b837dbd9565
Codeyg12 <codey.gallup@gmail.com>
2024-02-22 15:28:57 -0700

    automate postgres restart as well as prisma migrations

--- nestjs-api/src/auth/auth.service.ts
+++ nestjs-api/src/auth/auth.service.ts
@@ -1,32 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-import * as argon from 'argon2'
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  
+
   async signup(dto: AuthDto) {
     // generate the password hash
-    const hash = await argon.hash(dto.password)
+    const hash = await argon.hash(dto.password);
 
-    // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash
+    try {
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      // return the saved user
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
       }
-    })
-
-    delete user.hash
-
-    // return the saved user
-    return user
+      throw err;
+    }
   }
 
-  login() {
-    return { msg: 'LOGIN' };
-  }
+  async login(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if user does not exist throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    // compare password
+    const pMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!pMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
 
+    delete user.hash;
+    //send back user
+    return user;
+  }
 }

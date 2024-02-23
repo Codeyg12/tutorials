c6cb715119da7656b96c964c8426243978a3ee42
Codeyg12 <codey.gallup@gmail.com>
2024-02-22 20:28:59 -0700

    updating scripts for testings, changing E2E testing

--- nestjs-api/src/user/user.controller.ts
+++ nestjs-api/src/user/user.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
-import { GetUser } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
-import { User } from '@prisma/client'
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { User } from '@prisma/client';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
   getMe(@GetUser() user: User) {
-    return user
+    return user;
   }
 
   @Patch()

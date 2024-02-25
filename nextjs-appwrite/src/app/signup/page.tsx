0457528435bb84084182eae39192b8aa9a3ac0e7
Codeyg12 <codey.gallup@gmail.com>
2024-02-25 16:02:20 -0700

    db errors

--- nextjs-appwrite/src/app/signup/page.tsx
+++ nextjs-appwrite/src/app/signup/page.tsx
@@ -4,19 +4,46 @@ import React from "react";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   });
+  const [buttonDisabled, setButtonDiabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
-  const onSignup = async () => {};
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("api/signup", user);
+      console.log("Signup success", response.data);
+      router.push("/login");
+    } catch (err: any) {
+      console.error("Signup failed", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDiabled(false);
+    } else {
+      setButtonDiabled(true);
+    }
+  }, [user]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Sign up</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-black">
+      <h1 className="text-white">{loading ? "Processing..." : "Sign up"}</h1>
       <hr />
       <label htmlFor="username">Username</label>
       <input
@@ -40,18 +67,20 @@ export default function SignupPage() {
       <input
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         id="password"
-        type="text"
+        type="password"
         value={user.password}
         onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="Password"
       />
       <button
         onClick={onSignup}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus: border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 text-white focus:outline-none focus: border-gray-600"
       >
-        Sign Up
+        {buttonDisabled ? "Unable to sign up" : "Sign up"}
       </button>
-      <Link href="/login">Go to login page</Link>
+      <Link className="text-white" href="/login">
+        Go to login page
+      </Link>
     </div>
   );
 }
--- nextjs-appwrite/src/dbConfig/dbConfig.ts
+++ nextjs-appwrite/src/dbConfig/dbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URL!);
+    mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/nextjsAppwrite');
     const connection = mongoose.connection;
 
     connection.on("connected", () => {

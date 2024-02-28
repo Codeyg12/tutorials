c1c02c1e9cce760447af8c7b7ed400111981dc42
Codeyg12 <codey.gallup@gmail.com>
2024-02-28 11:29:46 -0700

    adding 'feed' of main page, added a skeleton on loading

---
+++ react-insta-clone/src/components/FeedPosts/FeedPosts.jsx
@@ -0,0 +1,48 @@
+import {
+  Container,
+  Flex,
+  Skeleton,
+  SkeletonCircle,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import FeedPost from "./FeedPost";
+
+const FeedPosts = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+  return (
+    <Container maxW={"container.sm"} py={10} px={2}>
+      {isLoading ? (
+        [0, 1, 2, 3].map((_, idx) => (
+          <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+            <Flex gap={2}>
+              <SkeletonCircle size={10} />
+              <VStack gap={2} alignItems={"flex-start"}>
+                <Skeleton height={"10px"} w={"200px"} />
+                <Skeleton height={"10px"} w={"200px"} />
+              </VStack>
+            </Flex>
+            <Skeleton w={"full"}>
+              <Box h={"500px"}>contents wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))
+      ) : (
+        <>
+          <FeedPost username="cmoney" avatar="/img1.png" img="/img1.png" />
+          <FeedPost username="nickarroni" avatar="/img2.png" img="/img2.png" />
+          <FeedPost username="roof" avatar="/img3.png" img="/img3.png" />
+          <FeedPost username="NYPD" avatar="/img4.png" img="/img4.png" />
+        </>
+      )}
+    </Container>
+  );
+};
+
+export default FeedPosts;

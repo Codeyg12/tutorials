aa8dd8c04d1ef5427a9a7a94db016c6c87b4c6c1
Codeyg12 <codey.gallup@gmail.com>
2024-02-28 19:46:44 -0700

    different files for login and signup, improved password input, custom hooks

---
+++ react-insta-clone/src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "insta-clone-21c28.firebaseapp.com",
+  projectId: "insta-clone-21c28",
+  storageBucket: "insta-clone-21c28.appspot.com",
+  messagingSenderId: "171718470807",
+  appId: "1:171718470807:web:afb3058334ffbd2b23cdd2",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, firestore, storage };

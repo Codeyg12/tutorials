84341c5349293bec1e0e69e90de822156b509455
Codeyg12 <codey.gallup@gmail.com>
2024-03-01 12:08:44 -0700

    deleting posts from page and decrement post count

--- react-insta-clone/src/components/Profile/ProfilePost.jsx
+++ react-insta-clone/src/components/Profile/ProfilePost.jsx
@@ -15,7 +15,7 @@ import {
   VStack,
   useDisclosure,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
@@ -23,11 +23,45 @@ import Comment from "../Comment/Comment";
 import PostFooter from "../FeedPosts/PostFooter";
 import useUserProfileStore from "../../store/userProfileStore";
 import useAuthStore from "../../store/authStore";
+import useShowToast from "../../hooks/useShowToast";
+import { deleteObject, ref } from "firebase/storage";
+import { firestore, storage } from "../../firebase/firebase";
+import { arrayRemove, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import usePostStore from "../../store/postStore";
 
 const ProfilePost = ({ post }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const userProfile = useUserProfileStore((state) => state.userProfile);
   const authUser = useAuthStore((state) => state.user);
+  const showToast = useShowToast();
+  const [isDeleting, setIsDeleting] = useState(false);
+  const deletePost = usePostStore((state) => state.deletePost);
+  const decrementPostsCount = useUserProfileStore((state) => state.deletePost);
+
+  const handleDeletePost = async () => {
+    if (!window.confirm("Are you sure you want to delete this post?")) return;
+    if (isDeleting) return;
+
+    try {
+      const imageRef = ref(storage, `posts/${post.id}`);
+      await deleteObject(imageRef);
+      const userRef = doc(firestore, "users", authUser.uid);
+      await deleteDoc(doc(firestore, "posts", post.id));
+
+      await updateDoc(userRef, {
+        posts: arrayRemove(post.id),
+      });
+
+      deletePost(post.id);
+      decrementPostsCount(post.id);
+
+      showToast("success", "Post deleted successfully", "success");
+    } catch (error) {
+      showToast("error", "Failed to delete post", "error");
+    } finally {
+      setIsDeleting(false);
+    }
+  };
 
   return (
     <>
@@ -134,6 +168,8 @@ const ProfilePost = ({ post }) => {
                       p={1}
                       size={"sm"}
                       bg={"transparent"}
+                      onClick={handleDeletePost}
+                      isLoading={isDeleting}
                     >
                       <MdDelete size={20} cursor={"pointer"} />
                     </Button>
--- react-insta-clone/src/store/postStore.js
+++ react-insta-clone/src/store/postStore.js
@@ -3,9 +3,9 @@ import { create } from "zustand";
 const usePostStore = create((set) => ({
   posts: [],
   createPost: (post) => set((state) => ({ posts: [post, ...state.posts] })),
-  // delete post
+  deletePost: (id) =>
+    set((state) => ({ posts: state.posts.filter((post) => post.id !== id) })),
   // add comment
-  //set posts
   setPosts: (posts) => set({ posts }),
 }));
 

3c75cd80adad26f54bb11e60a4d60afee7bf1491
Codeyg12 <codey.gallup@gmail.com>
2024-03-01 17:27:18 -0700

    env vars

--- react-insta-clone/src/firebase/firebase.js
+++ react-insta-clone/src/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "", //! DELETE B4 SAVING
-  authDomain: "insta-clone-21c28.firebaseapp.com",
-  projectId: "insta-clone-21c28",
-  storageBucket: "insta-clone-21c28.appspot.com",
-  messagingSenderId: "171718470807",
-  appId: "1:171718470807:web:afb3058334ffbd2b23cdd2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);

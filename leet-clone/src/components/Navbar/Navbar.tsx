8686bf4b68d71b71b6e6541e61c90046344c704e
Codeyg12 <codey.gallup@gmail.com>
2024-03-04 20:05:39 -0700

    creating navbar and link

--- leet-clone/src/components/Navbar/Navbar.tsx
+++ leet-clone/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 type NavbarProps = {
-    
+
 }
 
 const Navbar:React.FC<NavbarProps> = () => {
   return (
-    <div>scoop</div>
+    <div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
+        <Link href={'/'} className='flex items-center justify-center h-20'>
+            <img src='/logo.png' alt='Leet Clone' className='h-full' />
+        </Link>
+    </div>
   )
 }
 

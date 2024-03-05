67e8064510b8c47fbdc4b924adf5c705e5563a58
Codeyg12 <codey.gallup@gmail.com>
2024-03-04 19:06:58 -0700

    working out the router

--- leet-clone/src/app/page.tsx
+++ leet-clone/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       Hello World
+      <Navbar />
     </main>
   );
 }
---
+++ leet-clone/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type NavbarProps = {
+    
+}
+
+const Navbar:React.FC<NavbarProps> = () => {
+  return (
+    <div>scoop</div>
+  )
+}
+
+export default Navbar

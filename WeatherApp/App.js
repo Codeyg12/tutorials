a3347ed5b075ab304ac8ead2fa58dcfb2e109601
Codeyg12 <codey.gallup@gmail.com>
2024-03-14 14:13:54 -0600

    adding a safeview

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
+    <SafeAreaView style={styles.wrapper}>
     <View style={styles.container}>
-      <Text>Codey</Text>
+      <Text>Current Weather</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
+
+//* After using Expo app or QR code to run the app, the app will be displayed on the mobile device. Shaking the device brings up the developer menu. Select "Debug Remote JS" to open the app in the browser. The app will be displayed in the browser and the developer tools can be used to inspect the app.
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  wrapper: {
+    flex: 1,
+  }
 });

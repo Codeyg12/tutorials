0c41579c4501a2d366c16907b64550ed631bc128
Codeyg12 <codey.gallup@gmail.com>
2024-03-16 13:47:12 -0600

    adding tab navigation and styling, left options for top and bottom

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,25 +1,72 @@
-import { StatusBar, StyleSheet, View } from 'react-native'
+import { StatusBar } from 'react-native'
 import UpcomingWeather from './src/screens/UpcomingWeather'
 import CurrentWeather from './src/screens/CurrentWeather'
 import City from './src/screens/City'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { Feather } from '@expo/vector-icons'
+
+// Top Tab Navigator with sliding animation
+const Tab = createMaterialTopTabNavigator()
+
+// Bottom Tab Navigator with fade animation
+// const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <StatusBar barStyle={'light-content'} />
-      <CurrentWeather />
-      {/* <UpcomingWeather /> */}
-      {/* <City /> */}
-    </View>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray'
+        }}
+      >
+        <Tab.Screen
+          name={'Current'}
+          component={CurrentWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="droplet"
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name={'Upcoming'}
+          component={UpcomingWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="clock"
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name={'City'}
+          component={City}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="home"
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
 //* After using Expo app or QR code to run the app, the app will be displayed on the mobile device. Shaking the device brings up the developer menu. Select "Debug Remote JS" to open the app in the browser. The app will be displayed in the browser and the developer tools can be used to inspect the app.
 
 //* Sometimes you have to add and then remove a <StatusBar> component to get the safe area to work properly.
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})

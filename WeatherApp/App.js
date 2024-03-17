d9183b44a6c434d74d8416a65f07e27abebb35d3
Codeyg12 <codey.gallup@gmail.com>
2024-03-16 18:42:20 -0600

    adding a spinning loader

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,8 +1,19 @@
-import { StatusBar } from 'react-native'
+import { ActivityIndicator, StatusBar, StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
+import { useState } from 'react'
 
 export default function App() {
+  const [loading, setLoading] = useState(true)
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color={'blue'} />
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
       <StatusBar barStyle={'light-content'} />
@@ -14,3 +25,10 @@ export default function App() {
 //* After using Expo app or QR code to run the app, the app will be displayed on the mobile device. Shaking the device brings up the developer menu. Select "Debug Remote JS" to open the app in the browser. The app will be displayed in the browser and the developer tools can be used to inspect the app.
 
 //* Sometimes you have to add and then remove a <StatusBar> component to get the safe area to work properly.
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})

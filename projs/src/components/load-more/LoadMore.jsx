8f7d9c858ed572b580c6cbfeb83d4f0f6aa67c79
Codeyg12 <codey.gallup@gmail.com>
2024-03-20 19:57:23 -0600

    load more

--- projs/src/components/load-more/LoadMore.jsx
+++ projs/src/components/load-more/LoadMore.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./LoadMore.css";
 
 const LoadMore = () => {
-  return (
-    <div>LoadMore</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disable, setDisable] = useState(false);
 
-export default LoadMore
+  const fetchPosts = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const data = await res.json();
+
+      if (data && data.products && data.products.length > 0) {
+        setProducts((prev) => [...prev, ...data.products]);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, [count]);
+
+  useEffect(() => {
+    if (products && products.length === 100) setDisable(true);
+  }, [products]);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="btn-container">
+        <button disabled={disable} onClick={() => setCount(count + 1)}>
+          Load More Products
+        </button>
+        {disable && <p>No more products to load</p>}
+      </div>
+    </div>
+  );
+};
+
+export default LoadMore;

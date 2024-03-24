eac3782946c0895e3f59ba7a69582e5aedcc8688
Codeyg12 <codey.gallup@gmail.com>
2024-03-24 12:30:36 -0600

    some more functions and state added

--- JS/gorilla-game/main.js
+++ JS/gorilla-game/main.js
@@ -8,10 +8,38 @@ canvas.height = window.innerHeight;
 newGame();
 
 function newGame() {
-  state = {};
+  state = {
+    phase: 'aiming',
+    currentPlayer: 1,
+    bomb: {
+        x: undefined,
+        y: undefined,
+        rotation: 0,
+        velocity: {x: 0, y: 0}
+    },
+    backgroundBuildings: [],
+    buildings: [],
+    blastHoles: [],
+  };
+
+  for (let i = 0; i < 10; i++) {
+    generateBackgroundBuilding(i);
+  }
+  
+  for (let i = 0; i < 10; i++) {
+    generateBuilding(i);
+  }
+
+  initalizeBombPosition();
 
   draw();
 }
+
+function generateBackgroundBuilding(i) {}
+
+function generateBuilding(i) {}
+
+function initalizeBombPosition() {}
 
 function draw() {}
 

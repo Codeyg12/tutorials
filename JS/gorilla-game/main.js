7b25ea6e06dd96be1936aec79226fb15e4932302
Codeyg12 <codey.gallup@gmail.com>
2024-03-25 15:56:57 -0600

    drawing the main buildings that will be in play

--- JS/gorilla-game/main.js
+++ JS/gorilla-game/main.js
@@ -53,7 +53,37 @@ function generateBackgroundBuilding(i) {
   state.backgroundBuildings.push({ x, width, height });
 }
 
-function generateBuilding(i) {}
+function generateBuilding(i) {
+  const previousBuilding = state.buildings[i - 1];
+
+  const x = previousBuilding
+    ? previousBuilding.x + previousBuilding.width + 4
+    : 0;
+
+  const minWidth = 80;
+  const maxWidth = 130;
+  const width = Math.random() * (maxWidth - minWidth) + minWidth;
+
+  const platformWithGorilla = i === 1 || i === 6;
+
+  const minHeight = 40;
+  const maxHeight = 300;
+  const minHeightWithGorilla = 30;
+  const maxHeightWithGorilla = 150;
+
+  const height = platformWithGorilla
+    ? Math.random() * (maxHeightWithGorilla - minHeightWithGorilla) +
+      minHeightWithGorilla
+    : Math.random() * (maxHeight - minHeight) + minHeight;
+
+  const lightsOn = [];
+  for (let i = 0; i < 50; i++) {
+    const light = Math.random() <= 0.33 ? true : false;
+    lightsOn.push(light);
+  }
+
+  state.buildings.push({ x, width, height, lightsOn });
+}
 
 function initalizeBombPosition() {}
 
@@ -92,7 +122,42 @@ function drawBackgroundBuildings() {
   });
 }
 
-function drawBuildings() {}
+function drawBuildings() {
+  state.buildings.forEach((building) => {
+    ctx.fillStyle = "#4a3c68";
+    ctx.fillRect(building.x, 0, building.width, building.height);
+
+    const windowWidth = 10;
+    const windowHeight = 12;
+    const gap = 15;
+
+    const numOfFloors = Math.ceil(
+      (building.height - gap) / (windowHeight + gap)
+    );
+    const numOfRoomsPerFloor = Math.floor(
+      (building.width - gap) / (windowWidth + gap)
+    );
+
+    for (let floor = 0; floor < numOfFloors; floor++) {
+      for (let room = 0; room < numOfRoomsPerFloor; room++) {
+        if (building.lightsOn[floor * numOfRoomsPerFloor + room]) {
+          ctx.save();
+
+          ctx.translate(building.x + gap, building.height - gap);
+          ctx.scale(1, -1);
+
+          const x = room * (windowWidth + gap);
+          const y = floor * (windowHeight + gap);
+
+          ctx.fillStyle = "#ebb6a2";
+          ctx.fillRect(x, y, windowWidth, windowHeight);
+
+          ctx.restore();
+        }
+      }
+    }
+  });
+}
 
 function drawGorilla(player) {}
 

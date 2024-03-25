993f26d3984d13c7e487b292a347e14ff0905574
Codeyg12 <codey.gallup@gmail.com>
2024-03-25 15:37:59 -0600

    background is drawn

--- JS/gorilla-game/main.js
+++ JS/gorilla-game/main.js
@@ -35,7 +35,23 @@ function newGame() {
   draw();
 }
 
-function generateBackgroundBuilding(i) {}
+function generateBackgroundBuilding(i) {
+  const previousBuilding = state.backgroundBuildings[i - 1];
+
+  const x = previousBuilding
+    ? previousBuilding.x + previousBuilding.width + 4
+    : -30;
+
+  const minWidth = 60;
+  const maxWidth = 110;
+  const width = Math.random() * (maxWidth - minWidth) + minWidth;
+
+  const minHeight = 80;
+  const maxHeight = 350;
+  const height = Math.random() * (maxHeight - minHeight) + minHeight;
+
+  state.backgroundBuildings.push({ x, width, height });
+}
 
 function generateBuilding(i) {}
 
@@ -54,6 +70,33 @@ function draw() {
 
   ctx.restore();
 }
+
+function drawBackground() {
+  const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+  gradient.addColorStop(1, "#f8ba85");
+  gradient.addColorStop(0, "#ffc28e");
+
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "rgba(255, 255, 255, 0.6)";
+  ctx.beginPath();
+  ctx.arc(300, 420, 60, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function drawBackgroundBuildings() {
+  state.backgroundBuildings.forEach((building) => {
+    ctx.fillStyle = "#947285";
+    ctx.fillRect(building.x, 0, building.width, building.height);
+  });
+}
+
+function drawBuildings() {}
+
+function drawGorilla(player) {}
+
+function drawBomb() {}
 
 function throwBomb() {}
 

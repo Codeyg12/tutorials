3e0da948450b02873c3eacb380fa7cf0cd848b47
Codeyg12 <codey.gallup@gmail.com>
2024-03-28 17:21:35 -0600

    event listeners and fast forward / rewind

--- html5-video-player/main.js
+++ html5-video-player/main.js
@@ -2,6 +2,8 @@
 
 const playNpauseBtn = document.querySelector("#play-pause");
 const video = document.querySelector("video");
+const rewindBtn = document.querySelector("#rewind");
+const fastForwardBtn = document.querySelector("#fast-forward");
 
 function playNpauseFn() {
   video.paused ? video.play() : video.pause();
@@ -13,3 +15,15 @@ function updatePlayNPauseIcon() {
 
   icon.textContent = video.paused ? "play_arrow" : "paused";
 }
+
+function rewindNforwardFn(type) {
+  video.currentTime += type === "rewind" ? -10 : 10;
+}
+
+
+video.addEventListener("play", updatePlayNPauseIcon);
+video.addEventListener('click', playNpauseFn)
+video.addEventListener("pause", updatePlayNPauseIcon);
+playNpauseBtn.addEventListener("click", playNpauseFn);
+rewindBtn.addEventListener("click", () => rewindNforwardFn("rewind"));
+fastForwardBtn.addEventListener("click", () => rewindNforwardFn("forward"));

844b63ba245a8cde7be3522e0cc1a8c2082b59a0
Codeyg12 <codey.gallup@gmail.com>
2024-04-01 08:58:44 -0600

    last half finished

--- interview-algos/main.js
+++ interview-algos/main.js
@@ -1,3 +1,4 @@
+//* Reverese a String
 // function reverse(str) {
 //! First One
 // let reversed = '';
@@ -11,6 +12,7 @@
 // }
 // console.log(reverse('Hello'))
 
+//* Palindrome
 // function palindrome(word) {
 //     let newStr = word.split('').reverse().join('')
 
@@ -21,6 +23,7 @@
 
 // console.log(palindrome('kayak'));
 
+//* Most Appearing Character
 // function maxChar(str) {
 //   const charMap = {};
 //   let maxNum = 0;
@@ -41,6 +44,7 @@
 
 // console.log(maxChar("alongtimeagoinagalaxy"));
 
+//* Array Chunking
 // function chunk(arr, size) {
 //   let result = [];
 //   let index = 0;
@@ -52,3 +56,172 @@
 // }
 
 // console.log(chunk([1, 2, 3, 4, 5, 6], 2));
+
+//* Capitalize the First Letter
+// function capitalize(str) {
+//! First One
+//   let sentence = str.split(" ");
+//   let result = [];
+//   for (let char of sentence) {
+//     result.push(char.charAt(0).toUpperCase() + char.slice(1));
+//   }
+//   return result.join(" ");
+
+//? Second One
+//   let sentence = str.split(" ");
+//   return sentence
+//     .map((word) => word[0].toUpperCase() + word.slice(1))
+//     .join(" ");
+// }
+
+// console.log(capitalize("hey whats up"));
+
+//* Anagram
+//! First One
+// function charMap(str) {
+//   const charMap = {};
+//   str = str.toLowerCase().replace(/[\W]/g, "");
+//   for (let char of str) {
+//     charMap[char] = ++charMap[char] || 1;
+//   }
+//   return charMap;
+// }
+
+// function anagrams(strA, strB) {
+//   const charMapA = charMap(strA);
+//   const charMapB = charMap(strB);
+
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length)
+//     return false;
+
+//   for (let key in charMapA) {
+//     if (charMapA[key] !== charMapB[key]) return false;
+//   }
+
+//   return true;
+// }
+// console.log(anagrams("rail safety", "fairy tales"));
+//? Seccond One
+// function cleanStr(str) {
+//   return str.toLowerCase().replace(/[\W]/g, "").split("").sort().join("");
+// }
+// function anagram(strA, strB) {
+//   return cleanStr(strA) === cleanStr(strB);
+// }
+
+// console.log(anagram("rail safety", "fairy tales"));
+
+//* Number of Vowels
+// function vowels(str) {
+//! First One
+// const matches = str.match(/[aeiou]/gi)
+// return matches ? matches.length : 0
+//   let vowelCheck = ["a", "e", "i", "o", "u"];
+
+//   let count = 0;
+
+//   for (let char of str) {
+//     if (vowelCheck.includes(char)) count++;
+//   }
+
+//   return count;
+// }
+
+// console.log(vowels("hello"));
+
+//* FizzBuzz
+// function fizzBuzz(n) {
+//   for (let i = 1; i < n; i++) {
+//     if (i % 5 === 0 && i % 3 === 0) {
+//       console.log("fizzBuzz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// console.log(fizzBuzz(17));
+
+//* Level Steps
+// function steps(n) {
+//   for (let row = 1; row <= n; row++) {
+//     let line = "";
+//     for (let col = 1; col <= n; col++) {
+//       if (col <= row) {
+//         line += "#";
+//       }
+//     }
+//     console.log(line);
+//   }
+// }
+
+// steps(3);
+
+//* Pyramid
+// function pyramid(n) {
+//   const mid = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let line = "";
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (col >= mid - row && col <= mid + row) {
+//         line += "#";
+//       } else {
+//         line += " ";
+//       }
+//     }
+//     console.log(line);
+//   }
+// }
+
+// pyramid(3);
+
+//* Spiral Matrix
+// function matrix(n) {
+//   const result = [];
+//   let counter = 1,
+//     startRow = 0,
+//     endRow = n - 1,
+//     startCol = 0,
+//     endCol = n - 1;
+//   for (let i = 0; i < n; i++) {
+//     result.push([]);
+//   }
+
+//   while (startRow <= endRow && startCol <= endCol) {
+//     //Top
+//     for (let i = startCol; i <= endCol; i++) {
+//       result[startRow][i] = counter;
+//       counter++;
+//     }
+//     startRow++;
+
+//     //Right
+//     for (let i = startRow; i <= endRow; i++) {
+//       result[i][endCol] = counter;
+//       counter++;
+//     }
+//     endCol--;
+
+//     //Bottom
+//     for (let i = endCol; i >= startCol; i--) {
+//       result[endRow][i] = counter;
+//       counter++;
+//     }
+//     endRow--;
+
+//     //Left
+//     for (let i = endRow; i >= startRow; i--) {
+//       result[i][startCol] = counter;
+//       counter++;
+//     }
+//     startCol++;
+//   }
+
+//   return result;
+// }
+
+// console.log(matrix(4));

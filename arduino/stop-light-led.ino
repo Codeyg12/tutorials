930ceb49ec6d15d4ccf893e4e19c1a68cc550809
Codeyg12 <codey.gallup@gmail.com>
2024-04-06 12:36:10 -0600

    second arduino

--- arduino/stop-light-led.ino
+++ arduino/stop-light-led.ino
@@ -24,3 +24,42 @@
 //   delay(time);
 //   digitalWrite(pin, LOW);
 // }
+
+// //* Inputting colors using the serial monitor
+// int redPin = 3;
+// int greenPin = 6;
+// int bluePin = 9;
+// int colorR = 100;
+// int colorG = 192;
+// int colorB = 23;
+
+// String setColor;
+
+// void setup() {
+//   pinMode(redPin, OUTPUT);
+//   pinMode(greenPin, OUTPUT);
+//   pinMode(bluePin, OUTPUT);
+
+//   Serial.begin(9600);
+// }
+
+// void loop() {
+
+//   setColor = Serial.readString();
+//   Serial.print(setColor);
+//   Serial.print("hello");
+
+//   if (setColor == "red\n") {
+//     analogWrite(redPin, 255);
+//     analogWrite(greenPin, 0);
+//     analogWrite(bluePin, 0);
+//   } else if (setColor == "green\n") {
+//     analogWrite(redPin, 0);
+//     analogWrite(greenPin, 255);
+//     analogWrite(bluePin, 0);
+//   } else if (setColor == "blue\n") {
+//     analogWrite(redPin, 0);
+//     analogWrite(greenPin, 0);
+//     analogWrite(bluePin, 255);
+//   }
+// }

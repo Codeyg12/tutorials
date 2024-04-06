954e8d32d0128537480d39e92f7f5f48098e4d40
Codeyg12 <codey.gallup@gmail.com>
2024-04-06 11:26:51 -0600

    saving a setup for arduino

---
+++ arduino/stop-light-led.ino
@@ -0,0 +1,26 @@
+// //* StopLight
+// int DELAY_TIME = 3000;
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   pinMode(7, OUTPUT);
+//   pinMode(6, OUTPUT);
+//   pinMode(5, OUTPUT);
+//   pinMode(2, OUTPUT);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   digitalWrite(2, HIGH);
+//   lightBlink(7, DELAY_TIME);
+
+//   lightBlink(6, 1000);
+
+//   lightBlink(5, DELAY_TIME);
+// }
+
+// void lightBlink(char pin, int time) {
+//   digitalWrite(pin, HIGH);
+//   delay(time);
+//   digitalWrite(pin, LOW);
+// }

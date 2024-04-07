7084d00554042a2b955bd37e719281b081326516
Codeyg12 <codey.gallup@gmail.com>
2024-04-07 11:05:57 -0600

    year display

--- arduino/stop-light-led.ino
+++ arduino/stop-light-led.ino
@@ -64,118 +64,204 @@
 //   }
 // }
 
-//* Countdown 
-int DP = 3;
-int C = 4;
-int D = 5;
-int E = 6;
-int B = 7;
-int A = 8;
-int F = 9;
-int G = 10;
-int delayTime = 500;
+// //* Countdown 
+// int DP = 3;
+// int C = 4;
+// int D = 5;
+// int E = 6;
+// int B = 7;
+// int A = 8;
+// int F = 9;
+// int G = 10;
+// int delayTime = 500;
 
-void setup() {
-  pinMode(DP, OUTPUT);
-  pinMode(C, OUTPUT);
-  pinMode(D, OUTPUT);
-  pinMode(E, OUTPUT);
-  pinMode(B, OUTPUT);
-  pinMode(A, OUTPUT);
-  pinMode(F, OUTPUT);
-  pinMode(G, OUTPUT);
-}
+// void setup() {
+//   pinMode(DP, OUTPUT);
+//   pinMode(C, OUTPUT);
+//   pinMode(D, OUTPUT);
+//   pinMode(E, OUTPUT);
+//   pinMode(B, OUTPUT);
+//   pinMode(A, OUTPUT);
+//   pinMode(F, OUTPUT);
+//   pinMode(G, OUTPUT);
+// }
 
-void loop() {
-  // ZERO
-  digitalWrite(DP, LOW);
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, LOW);
-  delay(delayTime);
-  // ONE
-  digitalWrite(C, HIGH);
-  digitalWrite(D, LOW);
-  digitalWrite(E, LOW);
-  digitalWrite(B, HIGH);
-  digitalWrite(A, LOW);
-  digitalWrite(F, LOW);
-  digitalWrite(G, LOW);
-  delay(delayTime);
-  // TWO
-  digitalWrite(C, LOW);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(F, LOW);
-  digitalWrite(G, HIGH);
-  delay(delayTime);
-  // THREE
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(B, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(F, LOW);
-  digitalWrite(G, HIGH);
-  delay(delayTime);
-  // FOUR
-  digitalWrite(C, HIGH);
-  digitalWrite(D, LOW);
-  digitalWrite(E, LOW);
-  digitalWrite(B, HIGH);
-  digitalWrite(A, LOW);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  delay(delayTime);
-  // FIVE
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(B, LOW);
-  digitalWrite(A, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  delay(delayTime);
-  // SIX
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(A, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  delay(delayTime);
-  // SEVEN
-  digitalWrite(C, HIGH);
-  digitalWrite(D, LOW);
-  digitalWrite(E, LOW);
-  digitalWrite(B, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(F, LOW);
-  digitalWrite(G, LOW);
-  delay(delayTime);
-  // EIGHT
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  delay(delayTime);
-  // NINE
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(B, HIGH);
-  digitalWrite(A, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  delay(delayTime);
-}
+// void loop() {
+//   // ZERO
+//   digitalWrite(DP, LOW);
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, HIGH);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, HIGH);
+//   digitalWrite(G, LOW);
+//   delay(delayTime);
+//   // ONE
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, LOW);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, LOW);
+//   digitalWrite(F, LOW);
+//   digitalWrite(G, LOW);
+//   delay(delayTime);
+//   // TWO
+//   digitalWrite(C, LOW);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, HIGH);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, LOW);
+//   digitalWrite(G, HIGH);
+//   delay(delayTime);
+//   // THREE
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, LOW);
+//   digitalWrite(G, HIGH);
+//   delay(delayTime);
+//   // FOUR
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, LOW);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, LOW);
+//   digitalWrite(F, HIGH);
+//   digitalWrite(G, HIGH);
+//   delay(delayTime);
+//   // FIVE
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, LOW);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, HIGH);
+//   digitalWrite(G, HIGH);
+//   delay(delayTime);
+//   // SIX
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, HIGH);
+//   digitalWrite(B, LOW);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, HIGH);
+//   digitalWrite(G, HIGH);
+//   delay(delayTime);
+//   // SEVEN
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, LOW);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, LOW);
+//   digitalWrite(G, LOW);
+//   delay(delayTime);
+//   // EIGHT
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, HIGH);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, HIGH);
+//   digitalWrite(G, HIGH);
+//   delay(delayTime);
+//   // NINE
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, HIGH);
+//   digitalWrite(G, HIGH);
+//   delay(delayTime);
+// }
+
+// // 4-DIGIT YEAR
+// //? START TIME AT 1000, 500, 100, 5, 1
+// int A = 2;
+// int B = 7;
+// int C = 11;
+// int D = 9;
+// int E = 8;
+// int F = 3;
+// int G = 12;
+// int DP = 10;
+// int D1 = 4;
+// int D2 = 5;
+// int D3 = 6;
+// int D4 = 13;
+// int delayTime = 1;
+
+// void setup() {
+//   pinMode(A, OUTPUT);
+//   pinMode(B, OUTPUT);
+//   pinMode(C, OUTPUT);
+//   pinMode(D, OUTPUT);
+//   pinMode(E, OUTPUT);
+//   pinMode(F, OUTPUT);
+//   pinMode(G, OUTPUT);
+//   pinMode(DP, OUTPUT);
+//   pinMode(D1, OUTPUT);
+//   pinMode(D2, OUTPUT);
+//   pinMode(D3, OUTPUT);
+//   pinMode(D4, OUTPUT);
+// }
+
+// void loop() {
+//   // FIRST DIGIT
+//     digitalWrite(C, HIGH);
+//   digitalWrite(D, LOW);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, LOW);
+//   digitalWrite(F, LOW);
+//   digitalWrite(G, LOW);
+//   digitalWrite(D1, LOW);
+//   digitalWrite(D2, HIGH);
+//   digitalWrite(D3, HIGH);
+//   digitalWrite(D4, HIGH);
+//   delay(delayTime);
+//   // SECOND DIGIT
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, HIGH);
+//   digitalWrite(G, HIGH);
+//   digitalWrite(D1, HIGH);
+//   digitalWrite(D2, LOW);
+//   digitalWrite(D3, HIGH);
+//   digitalWrite(D4, HIGH);
+//   delay(delayTime);
+//   // THIRD DIGIT
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, HIGH);
+//   digitalWrite(G, HIGH);
+//   digitalWrite(D1, HIGH);
+//   digitalWrite(D2, HIGH);
+//   digitalWrite(D3, LOW);
+//   digitalWrite(D4, HIGH);
+//   delay(delayTime);
+//   // FOURTH DIGIT
+//   digitalWrite(C, HIGH);
+//   digitalWrite(D, HIGH);
+//   digitalWrite(E, LOW);
+//   digitalWrite(B, HIGH);
+//   digitalWrite(A, HIGH);
+//   digitalWrite(F, LOW);
+//   digitalWrite(G, HIGH);
+//   digitalWrite(D1, HIGH);
+//   digitalWrite(D2, HIGH);
+//   digitalWrite(D3, HIGH);
+//   digitalWrite(D4, LOW);
+//   delay(delayTime);
+// }

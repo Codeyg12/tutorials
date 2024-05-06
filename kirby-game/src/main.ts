7869a7a7871e46185808312d339d6b9138777010
Codeyg12 <codey.gallup@gmail.com>
2024-05-06 14:47:08 -0600

    Adding finally enemy

--- kirby-game/src/main.ts
+++ kirby-game/src/main.ts
@@ -1,4 +1,10 @@
-import { makeFlameEnemy, makeGuyEnemy, makePlayer, setControls } from "./entities";
+import {
+  makeBirdEnemy,
+  makeFlameEnemy,
+  makeGuyEnemy,
+  makePlayer,
+  setControls,
+} from "./entities";
 import { k } from "./kaboomCtx";
 import { makeMap } from "./utils";
 
@@ -42,20 +48,32 @@ async function gameSetup() {
       level1SpawnPoints.player[0].y
     );
 
-    setControls(k, kirb)
+    setControls(k, kirb);
     k.add(kirb);
     k.camScale(0.7, 0.7);
     k.onUpdate(() => {
       if (kirb.pos.x < level1Layout.pos.x + 432)
         k.camPos(kirb.pos.x + 500, 870);
     });
-    
+
     for (const flame of level1SpawnPoints.flame) {
-      makeFlameEnemy(k, flame.x, flame.y)
+      makeFlameEnemy(k, flame.x, flame.y);
     }
 
     for (const guy of level1SpawnPoints.guy) {
-      makeGuyEnemy(k, guy.x, guy.y)
+      makeGuyEnemy(k, guy.x, guy.y);
+    }
+
+    for (const bird of level1SpawnPoints.bird) {
+      const possibleSpeeds = [100, 200, 350];
+      k.loop(10, () => {
+        makeBirdEnemy(
+          k,
+          bird.x,
+          bird.y,
+          possibleSpeeds[Math.floor(Math.random() * possibleSpeeds.length)]
+        );
+      });
     }
   });
 

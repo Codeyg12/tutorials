329fd7ce57441a3bfedb0cfc717235db156f33e7
Codeyg12 <codey.gallup@gmail.com>
2024-07-10 15:21:01 -0600

    Adding QueryClient to update new images

--- my-photo-library/src/components/UploadButton.tsx
+++ my-photo-library/src/components/UploadButton.tsx
@@ -1,13 +1,25 @@
 "use client";
 
-import { CldUploadButton } from "next-cloudinary";
+import {
+  CldUploadButton,
+  CloudinaryUploadWidgetResults,
+} from "next-cloudinary";
 import { Upload } from "lucide-react";
+import { useResources } from "@/hooks/use-resource";
+import { CloudinaryResource } from '@/types/cloudinary'
 
 const UploadButton = () => {
+  const { addResources } = useResources();
+  function handleOnSuccess(results: CloudinaryUploadWidgetResults) {
+    addResources([results.info as CloudinaryResource])
+        
+  }
+
   return (
     <CldUploadButton
       signatureEndpoint={"/api/sign-cloudinary-params"}
       options={{ autoMinimize: true }}
+      onSuccess={handleOnSuccess}
     >
       <span className="flex gap-2 items-center">
         <Upload className="w-4 h-4" /> Upload

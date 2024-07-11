c0ac23dfcdd78d6155c82fee7997d7ddf7ea4266
Codeyg12 <codey.gallup@gmail.com>
2024-07-11 16:17:53 -0600

    Adding more metadata to images

--- my-photo-library/src/app/resources/[assetId]/page.tsx
+++ my-photo-library/src/app/resources/[assetId]/page.tsx
@@ -10,7 +10,8 @@ cloudinary.config({
 
 async function Resource({ params }: { params: { assetId: string } }) {
   const { resources } = await cloudinary.api.resources_by_asset_ids(
-    params.assetId
+    params.assetId,
+    { tags: true }
   );
   return <MediaViewer resource={resources[0]} />;
 }
--- my-photo-library/src/types/cloudinary.ts
+++ my-photo-library/src/types/cloudinary.ts
@@ -1,6 +1,10 @@
 export interface CloudinaryResource {
+  bytes: number;
+  created_at: string;
+  format: string;
   height: number;
-  width: number;
   public_id: string;
   secure_url: string;
+  tags: Array<string>;
+  width: number;
 }
